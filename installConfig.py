075f485d2fd8e76e706d8e674b79b431c605d0da
Toni Cebrián <ancechu@gmail.com>
2013-05-09 11:38:33 +0200

    Added the installConfig.py script

---
+++ installConfig.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+home_dir = os.environ.get("HOME")
+
+def link_file(filename, hide_file=False):
+	target_filename = home_dir + "/" + filename
+	if hide_file:
+		dirname = os.path.dirname(filename)
+		new_filename = "."+os.path.basename(filename)
+		target_filename = home_dir + "/" + dirname + "/" + new_filename
+
+	if os.path.exists(target_filename):
+		option = raw_input(target_filename + " already exists. Do you want to erase it? ([y]/n)")
+		if option == 'n' or option == 'N':
+			print "Skipping..."
+			return
+		else:
+			os.remove(target_filename)
+	
+	os.symlink(filename,target_filename)
+
+
+# Copy the configuration
+link_file("gitignore",True)
+link_file("gitcofig",True)
+link_file("tmux.conf",True)
+
+link_file("bash_extensions",True)
+link_file("dircolors.ansi-dark",True)
+link_file("git-completion.bash",True)
+link_file("xmobarrc",True)
+link_file("Xmodmap",True)
+link_file("xsession",True)
