17336a6654d3f92beb19a4cad7d8da38486029bb
Toni Cebrián <ancechu@gmail.com>
2013-05-09 11:47:01 +0200

    Fixed some install config scripts

--- installConfig.py
+++ installConfig.py
@@ -2,6 +2,7 @@
 
 import os
 home_dir = os.environ.get("HOME")
+git_dir = os.getcwd()
 
 def link_file(filename, hide_file=False):
 	target_filename = home_dir + "/" + filename
@@ -10,20 +11,20 @@ def link_file(filename, hide_file=False):
 		new_filename = "."+os.path.basename(filename)
 		target_filename = home_dir + "/" + dirname + "/" + new_filename
 
-	if os.path.exists(target_filename):
-		option = raw_input(target_filename + " already exists. Do you want to erase it? ([y]/n)")
+	if os.path.lexists(target_filename):
+		option = raw_input(target_filename + " already exists. Do you want to erase it? ([y]/n): ")
 		if option == 'n' or option == 'N':
 			print "Skipping..."
 			return
 		else:
 			os.remove(target_filename)
 	
-	os.symlink(filename,target_filename)
+	os.symlink(git_dir + "/" + filename,target_filename)
 
 
 # Copy the configuration
 link_file("gitignore",True)
-link_file("gitcofig",True)
+link_file("gitconfig",True)
 link_file("tmux.conf",True)
 
 link_file("bash_extensions",True)
