85ae16232ff6a0b3846d92a55ce4687632cc1a05
Toni Cebrián <toni.cebrian@gmail.com>
2018-02-28 21:09:23 +0100

    Add purge kafka to the set of utils

---
+++ bin/purge-kafka.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Plese provide topic to purge...."
+    exit 1
+fi
+
+KAFKA_FOLDER=/Users/cebrian/software/kafka
+echo "Setting message retention to 500ms"
+$KAFKA_FOLDER/bin/kafka-configs.sh --zookeeper localhost:2181 --entity-type topics --entity-name $1 --alter --add-config retention.ms=500
+
+echo "Waiting for the purge to happen" && sleep 1
+
+echo "Set retention to 10 minutes"
+$KAFKA_FOLDER/bin/kafka-configs.sh --zookeeper localhost:2181 --entity-type topics --entity-name $1 --alter --add-config retention.ms=600000
+
